2cc1717ab79a598cb87247075ed5739c94954ba0
Dan <dan.steffen.de@gmail.com>
2018-10-28 07:09:39 +0100

    weiterarbeiten am Grundgerüst

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,15 @@
 from app import app
 from flask import render_template
-from settings import Settings
+from app.settings import Settings
+import os, sys
+import markdown2
 
 rs = Settings()
-bla = rs.readConfig();
+data_dir = rs.get_data_dir()
 
 @app.route('/')
 def index():
-    user="Dan"
-    return render_template('index.html', username=bla)
+    if os.path.exists(data_dir+"/README.md"):
+        web_content = markdown2.markdown_path(data_dir+"/README.md")
+
+    return render_template('index.html', content=web_content)
