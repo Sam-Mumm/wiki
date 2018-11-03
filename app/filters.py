066fea8371828a41d7174b693f80a8adfae73c92
Dan <dan.steffen.de@gmail.com>
2018-11-03 20:21:42 +0100

    Dummy-Filter für jinja-Filter unter Versionskontrolle gestellt (soll später die Bilder-Links fixen)

--- app/filters.py
+++ app/filters.py
@@ -1,5 +1,6 @@
-from .. import app
+from app import app
 
+## Dummy Methode - Zum Testen von custom filters in jinja
 @app.template_filter()
 def caps(text):
-    return text.uppercase()
+    return text.upper()
