89eb724d793370c54a140317716a2723c3552337
Dan <dan.steffen.de@gmail.com>
2018-11-03 20:22:40 +0100

    Grundstruktur vom Wiki aktualisiert

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 from app import routes
+from filters import caps
--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,30 @@
 from app import app
 from flask import render_template
-from app.settings import Settings
 import os, sys
 import markdown2
+from app.settings import Settings
+from app.navigation import Navigation
+from flaskext.markdown import Markdown
+Markdown(app)
 
+# get the Settings from wiki_config.json
 rs = Settings()
 data_dir = rs.get_data_dir()
+wiki_name = rs.get_wiki_name()
+
+# Object/Class for the navigation through the wiki content
+navi = Navigation(data_dir)
 
 @app.route('/')
-def index():
+def home():
     if os.path.exists(data_dir+"/README.md"):
-        web_content = markdown2.markdown_path(data_dir+"/README.md")
+        content = markdown2.markdown_path(data_dir+"/README.md")
+    return render_template('markdown_content.tmpl.html', content=content)
 
-    return render_template('index.html', content=web_content)
+@app.route('/index', defaults={'path': ''})
+@app.route('/index/<path:path>')
+def index(path):
+    
+    content=navi.list_dir(path)
+
+    return render_template('table_content.tmpl.html', wiki_name=wiki_name, content=content)
