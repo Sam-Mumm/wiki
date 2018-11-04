0700da76ce1d227ad42771d3c9501c5fca5cfbae
Dan <dan.steffen.de@gmail.com>
2018-11-04 06:51:39 +0100

    Meta-Informationen unter Versionskontrolle gestellt

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'wiki',
+    version = '1.0.1',
+    url = 'https://github.com/Sam-Mumm/wiki.git',
+    author = 'Dan',
+    author_email = 'dan.steffen.de@gmail.com',
+    description = 'Small Wiki with git support',
+    packages = find_packages(),
+    install_requires = ['flask == 1.0.2', 'Whoosh == 2.7.4', 'markdown2 == 2.3.6'],
+)
