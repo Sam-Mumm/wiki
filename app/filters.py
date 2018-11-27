924e3dd29f0f69070f0d75a5725821b19eaf5e58
Dan <dan.steffen.de@gmail.com>
2018-11-27 05:40:44 +0100

    Filter um Search/Ersetzen-Funktion zum ersten der Bilder-Links erweitert

--- app/filters.py
+++ app/filters.py
@@ -1,6 +1,10 @@
 from app import app
+import re
 
 ## Dummy Methode - Zum Testen von custom filters in jinja
 @app.template_filter()
 def caps(text):
-    return text.upper()
+    # (\.\/|\.\.\/)?([a-z.-_A-Z1-9]\/*[a-z.-_A-Z1-9]+)
+    text = re.sub(r'<img src="(\.\/|\.\.\/)?([a-z.-_A-Z1-9]\/*[a-z.-_A-Z1-9]+)" alt="([a-z.-_A-Z1-9]*)" />', '<img src="/static/data/\1" alt="\2" />', text)
+    return text
+#    return text.upper()
