93918304bf97ddf16ae248c838e76927dee49eed
Dan <dan.steffen.de@gmail.com>
2018-11-29 06:18:50 +0100

    Ersetzung der Links korrigiert

--- app/filters.py
+++ app/filters.py
@@ -1,10 +1,14 @@
 from app import app
+import flask
 import re
 
-## Dummy Methode - Zum Testen von custom filters in jinja
 @app.template_filter()
 def caps(text):
-    # (\.\/|\.\.\/)?([a-z.-_A-Z1-9]\/*[a-z.-_A-Z1-9]+)
-    text = re.sub(r'<img src="(\.\/|\.\.\/)?([a-z.-_A-Z1-9]\/*[a-z.-_A-Z1-9]+)" alt="([a-z.-_A-Z1-9]*)" />', '<img src="/static/data/\1" alt="\2" />', text)
+
+    # Get the Path to the data-dir
+    content_folder_path=flask.url_for('static',filename='data/')
+
+    # Replace all img-src-Attribut with the path to the data-dir
+    text = re.sub(r'<img src="(\.\/|\.\.\/)?([a-z.-_A-Z1-9]\/*[a-z.-_A-Z1-9]+)" alt="([a-z.-_A-Z1-9]*)" />', '<img src="'+content_folder_path+r'\2" alt="\3" />', text)
+
     return text
-#    return text.upper()
