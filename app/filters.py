65d5a32d918a69a62d2fbe10d5f4fbd0ad428a35
Dan <dan.steffen.de@gmail.com>
2018-12-01 06:17:20 +0100

    filter überarbeitet

--- app/filters.py
+++ app/filters.py
@@ -1,14 +1,24 @@
 from app import app
 import flask
+from flask import request
 import re
+from os import path
 
 @app.template_filter()
 def caps(text):
+    path_parts=[]
 
     # Get the Path to the data-dir
-    content_folder_path=flask.url_for('static',filename='data/')
+    path_parts.append(flask.url_for('static',filename='data'))
+
+    # Get the current context
+    path_parts.append(path.dirname(request.path))
+    
+    path_parts.append("/")
+
+    static_path=''.join(path_parts)
 
     # Replace all img-src-Attribut with the path to the data-dir
-    text = re.sub(r'<img src="(\.\/|\.\.\/)?([a-z.-_A-Z1-9]\/*[a-z.-_A-Z1-9]+)" alt="([a-z.-_A-Z1-9]*)" />', '<img src="'+content_folder_path+r'\2" alt="\3" />', text)
+    text = re.sub(r'<img src="([\.\/|\.\.\/]?[a-z.-_A-Z1-9]\/*[a-z.-_A-Z1-9]+)" alt="([a-z.-_A-Z1-9]*)" />', '<img src="'+static_path+r'\1" alt="\2" />', text)
 
     return text
