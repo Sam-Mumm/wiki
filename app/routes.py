9c470c82e5999f171d9979cb47081c12fedaadd7
Dan <dan.steffen.de@gmail.com>
2018-12-01 15:37:30 +0100

    Mit der Edit-funktion angefangen

--- app/routes.py
+++ app/routes.py
@@ -13,35 +13,12 @@ wiki_name = rs.get_wiki_name()
 # Object/Class for the navigation through the wiki content
 navi = Navigation(data_dir)
 
-# Route for the displaying of a single page
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def home(path):
 
-    if path:
-        full_path = os.path.join(data_dir, path)
-
-        if os.path.isfile(full_path+".md"):
-            content = markdown2.markdown_path(full_path+".md", extras=["tables", "fenced-code-blocks"])
-
-            return render_template('markdown_content.tmpl.html', content=content)
-        elif os.path.isdir(full_path):
-            return redirect(url_for('index'))
-        else:
-            return render_template('404.tmpl.html')
-
-    else:
-        if os.path.exists(data_dir+"/README.md"):
-            content = markdown2.markdown_path(data_dir+"/README.md", extras=["tables", "fenced-code-blocks"])
-        else:
-            content=""
-        return render_template('markdown_content.tmpl.html', content=content)
 
 # route for the Navigation
 @app.route('/index', defaults={'path': ''})
 @app.route('/index/<path:path>')
 def index(path):
-
     if path:
         full_path = os.path.join(data_dir,path)
 
@@ -57,4 +34,42 @@ def index(path):
         content = navi.list_dir(path)
         return render_template('table_content.tmpl.html', wiki_name=wiki_name, content=content)
 
+@app.route('/edit', defaults={'path': ''})
+@app.route('/edit/<path:path>')
+def edit(path):
+    navi = [
+        {'endpoint': 'index', 'path': '', 'name': 'Index'},
+    ]
 
+    return render_template('edit.tmpl.html', wiki_name=wiki_name, navi=navi)
+
+
+# Route for the displaying of a single page
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def home(path):
+    navi = [
+        {'endpoint': 'index', 'path': '', 'name': 'Index'},
+    ]
+
+    if path:
+        navi.append({'endpoint': 'edit', 'path': "/"+path, 'name': 'Bearbeiten'})
+        full_path = os.path.join(data_dir, path)
+
+        if os.path.isfile(full_path+".md"):
+            content = markdown2.markdown_path(full_path+".md", extras=["tables", "fenced-code-blocks"])
+
+            return render_template('markdown_content.tmpl.html', content=content, navi=navi)
+        elif os.path.isdir(full_path):
+            return redirect(url_for('index'))
+        else:
+            return render_template('404.tmpl.html')
+
+    else:
+        navi.append({'endpoint': 'edit', 'path': '', 'name': 'Bearbeiten'})
+
+        if os.path.exists(data_dir+"/README.md"):
+            content = markdown2.markdown_path(data_dir+"/README.md", extras=["tables", "fenced-code-blocks"])
+        else:
+            content=""
+        return render_template('markdown_content.tmpl.html', content=content, navi=navi)
