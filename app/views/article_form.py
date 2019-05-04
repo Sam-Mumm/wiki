5d565a36307263ded5f8ddfebd69d456871fe7d5
Dan <dan.steffen.de@gmail.com>
2019-05-04 11:07:17 +0200

    Aufnahme eines Formularfeldes für die Prüfsumme

--- app/views/article_form.py
+++ app/views/article_form.py
@@ -1,9 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import TextField, TextAreaField, SubmitField
-
+from wtforms import TextField, TextAreaField, SubmitField, HiddenField
 
 class ArticleForm(FlaskForm):
     path = TextField("Pfad", render_kw={"size": 45})
     comment = TextField("Kommentar", render_kw={"size": 40})
+    checksum = HiddenField()
+
     submit = SubmitField("Abschicken")
     article_content = TextAreaField("content", render_kw={"rows": 25, "cols": 100})
