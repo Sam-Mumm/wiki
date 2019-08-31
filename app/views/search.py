cc5e9475061b979acf1662ab78667d571a61e4b0
Dan <dan.steffen.de@gmail.com>
2019-08-31 07:51:53 +0200

    Suchfunktionen in ein eigenes Modul ausgelagert

--- app/views/search.py
+++ app/views/search.py
@@ -1,49 +1,18 @@
 from flask import Blueprint, render_template, current_app, request, redirect, url_for
-from whoosh.fields import *
-import whoosh.index
-from whoosh.qparser import QueryParser
-import os, sys
-from whoosh.filedb.filestore import FileStorage
-import codecs
+from .whoosh_search import search_index, create_index
 
 pages_search = Blueprint("pages_search", __name__)
 
 @pages_search.before_app_first_request
-def refresh_search_db():
-
+def index_refresh():
     index_dir = current_app.config['INDEX_DIR']
     data_dir = current_app.config['DATA_DIR']
 
-    storage_obj = FileStorage(os.path.abspath(index_dir))
-
-    schema = Schema(path=ID(stored=True, unique=True), content=TEXT(stored=True))
-
-    # Schema of the index
-    idx = storage_obj.create_index(schema)
-
-    writer = idx.writer()
-
-    # Iterate over the data_dir and add the contents to the index
-    for (path, dirs, files) in os.walk(data_dir):
-        # Remove the git-Folder
-        if '.git' in dirs:
-            dirs.remove('.git')
-
-        for article in files:
-            if article.endswith('.md'):
-                article_path=path+"/"+article
-
-            # Get file content
-            with codecs.open(article_path, "r", "utf-8") as f:
-                content = f.read()
-                writer.add_document(path=article_path, content=content)
-    writer.commit()
-
+    create_index(index_dir, data_dir)
 
 @pages_search.route('/search', methods=["POST", "GET"])
 def search():
     results = []
-    results_tmpl = []
 
     navi_buttons = [
         {'endpoint': 'pages_index.index', 'path': '', 'name': 'Index'}
@@ -53,35 +22,10 @@ def search():
         msg = "Es wurde kein Suchbegriff angegeben"
         return render_template('search_results.tmpl.html', search_msg=msg, results=results, navi=navi_buttons)
 
-    index_dir = current_app.config['INDEX_DIR']
-
     search_str = request.form['search']
+    index_dir = current_app.config['INDEX_DIR']
+    data_dir = current_app.config['DATA_DIR']
 
-    storage_obj = FileStorage(os.path.abspath(index_dir))
+    msg, results = search_index(search_str, index_dir, data_dir)
 
-    try:
-        idx=storage_obj.open_index()
-    except whoosh.index.EmptyIndexError:
-        refresh_search_db
-        idx=storage_obj.open_index()
-
-    query_obj=QueryParser("content", idx.schema).parse(search_str)
-    searcher = idx.searcher()
-    results = searcher.search(query_obj)
-
-    if len(results) == 0:
-        msg = "Es gab fuer den Suchstring "+search_str+" keine Treffer"
-        return render_template('search_results.tmpl.html', search_msg=msg, results=results, navi=navi_buttons)
-
-    msg = "Es wurden "+str(len(results))+" Treffer fuer die Suchanfrage "+search_str+" gefunden"
-    # Aufbereitung der Daten
-    for r in results:
-        hit = {}
-
-        hit['path']="/".join(os.path.normpath(r['path']).split(os.path.sep)[3:])
-        hit['content']=r['content']
-
-        results_tmpl.append(hit)
-
-
-    return render_template('search_results.tmpl.html', search_msg=msg, results=results_tmpl, navi=navi_buttons)
+    return render_template('search_results.tmpl.html', search_msg=msg, results=results, navi=navi_buttons)
---
+++ app/views/whoosh_search.py
@@ -0,0 +1,74 @@
+from whoosh.fields import *
+import whoosh.index
+from whoosh.qparser import QueryParser
+import os, sys, shutil
+from whoosh.filedb.filestore import FileStorage
+import codecs
+
+def create_index(index_dir, data_dir):
+    index_dir_absolute = os.path.abspath(index_dir)
+    schema = Schema(path=ID(stored=True, unique=True), content=TEXT(stored=True))
+
+    storage_obj = FileStorage(index_dir_absolute)
+
+    if whoosh.index.exists_in(index_dir_absolute):
+        try:
+            shutil.rmtree(index_dir_absolute)
+            os.makedirs(index_dir_absolute)
+        except e:
+            return False
+
+    idx = storage_obj.create_index(schema)
+
+    writer = idx.writer()
+
+    for (path, dirs, files) in os.walk(data_dir):
+        # Remove the git-Folder
+        if '.git' in dirs:
+            dirs.remove('.git')
+
+        for article in files:
+            if article.endswith('.md'):
+                article_path=path+"/"+article
+
+#            # Get file content
+            with codecs.open(article_path, "r", "utf-8") as f:
+                content = f.read()
+                writer.add_document(path=article_path, content=content)
+
+    writer.commit()
+
+    return True
+
+
+def search_index(search_str, index_dir, data_dir):
+    msg = ""
+    result_set = []
+    index_dir_absolute = os.path.abspath(index_dir)
+
+    storage_obj = FileStorage(index_dir_absolute)
+
+    try:
+        idx=storage_obj.open_index()
+    except whoosh.index.EmptyIndexError:
+        create_index(index_dir, data_dir)
+        idx=storage_obj.open_index()
+
+    query_obj=QueryParser("content", idx.schema).parse(search_str)
+    searcher = idx.searcher()
+    results = searcher.search(query_obj)
+
+    if len(results) == 0:
+        msg = "Es gab fuer den Suchstring "+search_str+" keine Treffer"
+        return msg, result_set
+
+    msg = "Es wurden "+str(len(results))+" Treffer fuer die Suchanfrage "+search_str+" gefunden"
+    for r in  results:
+        hit = {}
+
+        hit['path'] = "/".join(os.path.normpath(r['path']).split(os.path.sep)[3:])
+        hit['content'] = r['content']
+
+        result_set.append(hit)
+
+    return msg, result_set
