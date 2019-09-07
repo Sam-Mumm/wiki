cc87d52c956290768ddc780d5cc456c2edf8a722
Dan <dan.steffen.de@gmail.com>
2019-09-07 12:46:24 +0200

    Fehlermeldung angepasst

--- app/views/file_io.py
+++ app/views/file_io.py
@@ -16,7 +16,7 @@ def readRaw(path):
         with codecs.open(path, 'r', 'utf-8') as fh:
             content = fh.read()
     except:
-        raise PermissionError("Der Artikel konnte nicht gelesen werden, bitte die Zugriffsrechte prüfen")
+        raise PermissionError("Der Artikel konnte nicht gelesen werden, bitte die Zugriffsrechte überprüfen")
 
     return content
 
@@ -27,7 +27,7 @@ def updateArticle(path, content):
         with codecs.open(path, 'w+', 'utf-8') as fh:
             fh.write(content)
     except:
-        raise PermissionError("Die Datei konnte nicht geschrieben werden, bitte die Zugriffsrechte prüfen")
+        raise PermissionError("Der Artikel konnte nicht geschrieben werden, bitte die Zugriffsrechte prüfen")
 
     return True
 
