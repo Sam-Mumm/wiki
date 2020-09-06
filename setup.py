7be5282dea6f95eba30cd107c8062a08f798b33d
Dan <dan.steffen.de@gmail.com>
2020-09-06 17:12:33 +0200

    Aktualisieren von den markdown2-Modul-Version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author_email = 'dan.steffen.de@gmail.com',
     description = 'Small Wiki with git support',
     packages = find_packages(),
-    install_requires = ['flask == 1.1.2', 'Whoosh == 2.7.4', 'markdown2 == 2.3.8', 'Flask-WTF==0.14.3', 'Flask-Babel==1.0.0'],
+    install_requires = ['flask == 1.1.2', 'Whoosh == 2.7.4', 'markdown2 == 2.3.9', 'Flask-WTF==0.14.3', 'Flask-Babel==1.0.0'],
     include_package_data = True,
     extras_require={ 'testing': ["pytest"] },
     scripts=["wiki_run.py"]
