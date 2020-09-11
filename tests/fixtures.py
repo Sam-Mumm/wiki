5a0115ea941b55bbfc1ae7f8581909bbbc346100
Dan <dan.steffen.de@gmail.com>
2020-09-11 22:02:08 +0200

    Verschieben der Fixtures

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -11,32 +11,36 @@ def tempdir():
     shutil.rmtree(tempdir)
 
 
-@pytest.fixture
-def app():
+@pytest.fixture()
+def app(tempdir):
     wiki = create_app()
+
+    wiki.config['DATA_DIR']=tempdir
+
     context = wiki.test_request_context()
+
     context.push()
 
-    yield wiki
+    yield wiki, tempdir
 
     context.pop()
 
 
 @pytest.yield_fixture()
 def test_client(app):
-    client = app.test_client()
-    return client
+    client = app[0].test_client()
+    return client, app[1]
 
 
-@pytest.fixture
+@pytest.fixture()
 def captured_templates(app):
     recorded = []
 
     def record(sender, template, context, **extra):
         recorded.append((template, context))
 
-    template_rendered.connect(record, app)
+    template_rendered.connect(record, app[0])
     try:
         yield recorded
     finally:
-        template_rendered.disconnect(record, app)
+        template_rendered.disconnect(record, app[0])
