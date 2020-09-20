2e52342b436066e6ff069a3d1e8a1ac45d06812d
Dan <dan.steffen.de@gmail.com>
2020-09-20 18:45:46 +0200

    Weiterschreiben der Testfälle

--- tests/test_view.py
+++ tests/test_view.py
@@ -18,7 +18,7 @@ articles = [
     ]
 
 
-# Test auf einem Wiki mit einem leeren Datenverzeichnis
+# Erfolgsfall: Test auf einem Wiki mit einem leeren Datenverzeichnis
 @pytest.mark.parametrize("article", articles)
 def test_wiki_empty(test_client, captured_templates, article):
     response = test_client[0].get(article['url'])
@@ -43,7 +43,7 @@ def test_wiki_empty(test_client, captured_templates, article):
     assert content['wiki_name'] == "My Testwiki"
 
 
-# Test auf einem Wiki mit einem leeren Datenverzeichnis
+# Erfolgsfall: Test von dem anzeigen von Artikeln
 @pytest.mark.parametrize("article", articles)
 def test_view_article(test_client, captured_templates, article):
     # Anlegen der zu verschiebenden Datei
@@ -63,3 +63,91 @@ def test_view_article(test_client, captured_templates, article):
     assert template.name == "markdown_content.tmpl.html"
     assert content['content'] == article['content_html']
     assert len(content['navi']) == 2
+
+
+@pytest.mark.parametrize("article", articles)
+def test_wiki_root_no_permissions(test_client, captured_templates, article):
+    # Anlegen der zu verschiebenden Datei
+    src_path=os.path.join(test_client[1], article['dir'])
+    os.makedirs(src_path, exist_ok=True)
+
+    with codecs.open(os.path.join(src_path, article['filename']), 'w', 'utf-8') as fh:
+        fh.write(article['content_md'])
+
+    os.chmod(test_client[1], 0o000)
+
+    response = test_client[0].get(article['url'])
+
+    response.status_code == 200
+
+    template, content = captured_templates[0]
+
+    if article['url'] == "/":
+        assert template.name == "markdown_content.tmpl.html"
+        assert content['content'] == "<h1>Willkommen</h1>"
+
+        # Anzahl der Buttons
+        assert len(content['navi']) == 2
+    else:
+        assert template.name == "404.tmpl.html"
+
+        # Anzahl der Buttons
+        assert len(content['navi']) == 0
+
+    assert content['wiki_name'] == "My Testwiki"
+
+    os.chmod(test_client[1], 0o700)
+
+
+@pytest.mark.parametrize("article", articles[1:])
+def test_wiki_subdir_no_permissions(test_client, captured_templates, article):
+    src_path=os.path.join(test_client[1], article['dir'])
+    os.makedirs(src_path, exist_ok=True)
+
+    with codecs.open(os.path.join(src_path, article['filename']), 'w', 'utf-8') as fh:
+        fh.write(article['content_md'])
+
+    os.chmod(src_path, 0o200)
+
+    response = test_client[0].get(article['url'])
+
+    response.status_code == 200
+
+    template, content = captured_templates[0]
+
+    assert template.name == "404.tmpl.html"
+
+    # Anzahl der Buttons
+    assert len(content['navi']) == 0
+
+    assert content['wiki_name'] == "My Testwiki"
+
+    os.chmod(src_path, 0o700)
+
+
+@pytest.mark.parametrize("article", articles)
+def test_wiki_article_no_permissions(test_client, captured_templates, article):
+    src_path=os.path.join(test_client[1], article['dir'])
+    os.makedirs(src_path, exist_ok=True)
+
+    with codecs.open(os.path.join(src_path, article['filename']), 'w', 'utf-8') as fh:
+        fh.write(article['content_md'])
+
+    os.chmod(os.path.join(src_path, article['filename']), 0o200)
+
+    response = test_client[0].get(article['url'])
+
+    response.status_code == 200
+
+    template, content = captured_templates[0]
+
+    if article['url'] == "/":
+        assert template.name == "markdown_content.tmpl.html"
+        assert len(content['navi']) == 2
+    else:
+        assert template.name == "404.tmpl.html"
+        assert len(content['navi']) == 0
+
+    assert content['wiki_name'] == "My Testwiki"
+
+    os.chmod(os.path.join(src_path, article['filename']), 0o700)
