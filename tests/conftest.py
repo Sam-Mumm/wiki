a13ee82216bf842daa693a7342017331431d502e
Dan <dan.steffen.de@gmail.com>
2024-01-04 18:14:46 +0100

    Testfälle für die View

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,7 @@ from tempfile import mkdtemp
 import shutil
 from wiki.constants import *
 import json
+import os
 
 @pytest.fixture(scope="session")
 def temp_datadir():
@@ -12,8 +13,8 @@ def temp_datadir():
     yield tempdir
     shutil.rmtree(tempdir)
 
-@pytest.fixture(scope="session")
-def app(request, temp_datadir):
+@pytest.fixture(scope="session", name="app")
+def app(request, temp_datadir, data_structure):
     app = create_app()
 
     create_testdata(app, temp_datadir, data_structure)
@@ -30,21 +31,23 @@ def client(request, app):
     return client
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="session", name="data_structure")
 def data_structure():
-    return read_file()
-
-
-def read_file():
-    with open("./data.json", "r") as f:
+    with open("data.json", "r") as f:
         return json.load(f)
 
 
 # Generieren von Testdaten basierend auf data.json
 def create_testdata(app, temp_datadir, data_structure):
-
-    print(data_structure)
-    # Anlegen der Startseite
+    # Anlegen der Startseite abhängig von der Konfigurationsdatei
     start_site="{}/{}{}".format(temp_datadir, app.config['START_SITE'], MARKDOWN_FILE_EXTENSION)
     with open(start_site, "w") as f:
         f.write("# Startseite fuer Testdaten\n")
+
+    for e in data_structure:
+        dir_path="{}/{}".format(temp_datadir, e['path'])
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open("{}/{}".format(dir_path, e['filename']), 'w') as f:
+            f.write(e['content'])
+
